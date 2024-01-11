488d55803a8cae8e62830e415031dea3158e0f7e
Ryan <s10261024@connect.np.edu.sg>
2024-01-11 10:08:33 +0800

    add topping class

---
+++ S10261024_PRG2Assignment/S10261024_PRG2Assignment/Classes/Topping.cs
@@ -0,0 +1,21 @@
+﻿namespace S10261024_PRG2Assignment.Classes
+{
+    class Topping
+    {
+        private string type;
+
+        public string Type { get; set; }
+
+        public Topping() { }
+
+        public Topping(string type)
+        {
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return $"Topping Class:\nType: {Type}\n\n";
+        }
+    }
+}
