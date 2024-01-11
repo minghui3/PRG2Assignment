d42cf1cc382868f6d913f66d012b1e10cc63329e
Ryan <s10261024@connect.np.edu.sg>
2024-01-11 10:05:06 +0800

    add customer class

---
+++ S10261024_PRG2Assignment/S10261024_PRG2Assignment/Classes/Customer.cs
@@ -0,0 +1,57 @@
+﻿namespace S10261024_PRG2Assignment.Classes
+{
+    class Customer
+    {
+        private string name;
+        private int memberId;
+        private DateTime dob;
+        private Order currentOrder;
+        private List<Order> orderHistory;
+        private PointCard rewards;
+        public string Name { get; set; }
+        public int MemberId { get; set; }
+        public DateTime DOB { get; set; }
+        public Order CurrentOrder { get; set; }
+        public List<Order> OrderHistory { get; set; }
+        public PointCard Rewards { get; set; }
+
+        public Customer() { }
+
+        public Customer(string name, int id, DateTime dob)
+        {
+            Name = name; 
+            MemberId = id;
+            DOB = dob;
+
+            //need to double check these
+
+            CurrentOrder = new();
+            OrderHistory = new();
+            Rewards = new();
+        }
+
+        public Order MakeOrder()
+        {
+            //code to get user input
+            
+            return;
+        }
+
+        public bool IsBirthday()
+        {
+            DateTime now = DateTime.Now;
+
+            if (now.Month == DOB.Month && now.Day == DOB.Day)
+            {
+                return true;
+            }
+
+            return false; 
+        }
+
+        public override string ToString()
+        {
+            return $"Customer Variable:\nName: {Name}\nMemberId: {MemberId}\nDOB: {dob.Date}\nCurrent Order: {CurrentOrder}\nOrder History: {OrderHistory}\nRewards: {Rewards}\n\n";
+        }
+    }
+}
