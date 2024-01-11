8401c5289e1efe029719b94293baeb2cc3647d8c
Ryan <s10261024@connect.np.edu.sg>
2024-01-11 10:04:52 +0800

    add cup stage

---
+++ S10261024_PRG2Assignment/S10261024_PRG2Assignment/Classes/Cup.cs
@@ -0,0 +1,29 @@
+﻿namespace S10261024_PRG2Assignment.Classes
+{
+    class Cup : IceCream
+    {
+        public Cup() { }
+
+        public Cup(string opt, int scoops, List<Flavour> fList, List<Topping> tList) : base(opt, scoops, fList, tList) { }
+
+        public override double CalculatePrice()
+        {
+            //Scoops
+            double price = (Scoops == 1) ? 4 : (Scoops == 2) ? 5.5 : 6;
+
+            //Premium flavour
+            price += Flavours.Where(f => f.Premium).Sum(f => f.Quantity * 2);
+
+            //Toppings
+            price += Toppings.Count();
+
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return "Cup Variable:\n" + base.ToString() + "\n\n";
+        }
+
+    }
+}
