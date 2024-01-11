d4f372e1e7b3e5f2ed0a4e60f3eca80b84067e73
Ryan <s10261024@connect.np.edu.sg>
2024-01-11 10:05:19 +0800

    add flavour class

---
+++ S10261024_PRG2Assignment/S10261024_PRG2Assignment/Classes/Flavour.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace S10261024_PRG2Assignment.Classes
+{
+    class Flavour
+    {
+        private string type;
+        private bool premium;
+        private int quantity;
+
+        public string Type { get; set; }
+        public bool Premium { get; set; }
+        public int Quantity { get; set; }
+
+        public Flavour() { }
+
+        public Flavour(string type, bool premium, int quantity)
+        {
+            Type = type;
+            Premium = premium;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Flavour Variable:\nType: {Type}\nPremium: {Premium}\nQuantity: {Quantity}\n\n";
+        }
+    }
+}
