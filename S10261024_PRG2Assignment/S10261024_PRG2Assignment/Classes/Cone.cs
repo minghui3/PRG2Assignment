80df8dd7effc1469817ec1ec7728686e8f6935eb
Ryan <s10261024@connect.np.edu.sg>
2024-01-11 09:57:27 +0800

    Add cone class

---
+++ S10261024_PRG2Assignment/S10261024_PRG2Assignment/Classes/Cone.cs
@@ -0,0 +1,38 @@
+﻿namespace S10261024_PRG2Assignment.Classes
+{
+    class Cone : IceCream
+    {
+        private bool dipped;
+
+        public bool Dipped;
+
+        public Cone() { }
+
+        public Cone(string opt, int scoops, List<Flavour> fList, List<Topping> tList, bool dipped) : base(opt, scoops, fList, tList)
+        {
+            Dipped = dipped;
+        }
+
+        public override double CalculatePrice()
+        { 
+            //Scoops
+            double price = (Scoops == 1) ? 4 : (Scoops == 2) ? 5.5 : 6;
+
+            //Chocolate-dipped
+            if (dipped) price += 2;
+
+            //Premium flavour
+            price += Flavours.Where(f => f.Premium).Sum(f => f.Quantity * 2);
+
+            //Toppings
+            price += Toppings.Count();
+
+            return price;
+        }
+
+        public override string ToString()
+        {
+            return "Cone Variable:\n" + base.ToString() + $"\nDipped: {Dipped}\n\n";
+        }
+    }
+}
